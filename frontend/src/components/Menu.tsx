214c41696c1112dd9ad6edbca24c8e7fc4c0e2ae
Thiago Santa Clara <strovsk@outlook.com>
2024-09-14 19:46:23 -0300

    feat: implement responsiveness config

--- frontend/src/components/Menu.tsx
+++ frontend/src/components/Menu.tsx
@@ -1,21 +1,58 @@
+'use client';
+
 import React from "react";
-import { Button, Container } from "@mui/material";
+import Grid from '@mui/material/Grid2';
+import { Button } from "@mui/material";
+import theme from "@/providers/theme";
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import Link from "next/link";
 
-
 export default function Menu() {
   return (
-    <Container fixed sx={{ position: 'fixed', bottom: '4%' }}>
-      <Link href="#">
-        <Button sx={{ textTransform: 'none' }} color="primary" variant="contained" endIcon={<KeyboardArrowUpIcon />}>Veja meu resumo</Button>
-      </Link>
-      <Link href="#thech-skills">
-        <Button sx={{ textTransform: 'none' }} color="primary" variant="text">tecnologias</Button>
-      </Link>
-      <Link href="#projects">
-        <Button sx={{ textTransform: 'none' }} color="primary" variant="text">projetos</Button>
-      </Link>
-    </Container>
+    <Grid container spacing={5} margin={5} sx={{
+      position: 'fixed',
+      bottom: 10,
+      [theme.breakpoints.down('sm')]: { margin: 0, width: '100%', top: 10 }
+    }}>
+      <Grid sx={{
+        [theme.breakpoints.down('sm')]: { order: 3 }
+      }}>
+        <Link href="#">
+          <Button
+            sx={{
+              textTransform: 'none',
+              [theme.breakpoints.down('sm')]: {
+                position: 'fixed',
+                bottom: 0,
+                width: '100%',
+                borderBottomRightRadius: 0,
+                borderBottomLeftRadius: 0,
+              }
+            }}
+            color="primary"
+            variant="contained"
+            endIcon={<KeyboardArrowUpIcon />}
+          >Veja meu resumo</Button>
+        </Link>
+      </Grid>
+
+      <Grid container justifyContent={'space-around'} sx={{
+        [theme.breakpoints.down('sm')]: { width: '100%' }
+      }}>
+
+        <Grid>
+          <Link href="#thech-skills">
+            <Button sx={{ textTransform: 'none' }} color="primary" variant="text">tecnologias</Button>
+          </Link>
+        </Grid>
+
+        <Grid>
+          <Link href="#projects">
+            <Button sx={{ textTransform: 'none' }} color="primary" variant="text">projetos</Button>
+          </Link>
+        </Grid>
+
+      </Grid>
+    </Grid>
   )
 }
