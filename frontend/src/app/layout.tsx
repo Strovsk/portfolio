5ae24124940fb0b1ee042611ca2f2b307f9e90be
Thiago Santa Clara <strovsk@outlook.com>
2024-09-14 11:31:10 -0300

    feat: add mui theme provider

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "@/providers/theme";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +15,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt">
+      <body>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+            {children}
+          </ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
---
+++ frontend/src/providers/theme.ts
@@ -0,0 +1,24 @@
+"use client";
+
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: "rgb(9, 23, 36)",
+			contrastText: "rgba(255, 255, 255)",
+		},
+		background: {
+			default: "rgb(238, 238, 238)",
+		},
+		text: {
+			primary: "rgb(27, 27, 27)",
+			secondary: "rgb(38, 38, 38)",
+		},
+		action: {
+			active: "blue",
+		},
+	},
+});
+
+export default theme;
