9fb886c62e5e907c93b0a958f0d7344f89d52a83
Thiago Santa Clara <strovsk@outlook.com>
2024-09-28 15:16:18 -0300

    feat: add animated background

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,19 @@
 import { Menu, WellcomeMessage } from "@/components";
+import { HandBackground } from "@/components";
+import { Container } from "@mui/material";
 
 export default function Home() {
-  return (
-    <main>
-      <Menu />
-      <WellcomeMessage />
-    </main>
-  );
+	return (
+		<main>
+			<Menu />
+			<Container
+				maxWidth={false}
+				sx={{ width: "100vw", height: "100vh" }}
+				disableGutters
+			>
+				<WellcomeMessage />
+				<HandBackground />
+			</Container>
+		</main>
+	);
 }
--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,4 +1,5 @@
 import Menu from "./Menu/Menu";
 import WellcomeMessage from "./WellcomeMessage/WellcomeMessage";
+import HandBackground from "./HandBackground/HandBackground";
 
-export { Menu, WellcomeMessage };
+export { Menu, WellcomeMessage, HandBackground };
