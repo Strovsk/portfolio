eddc37c89b084a9d6729d581ccbc37e3139f2c1c
Thiago Santa Clara <strovsk@outlook.com>
2024-12-22 23:16:15 -0300

    feat: remove validation middleware for tech skill listing route and add the post verb

--- backend/src/routes/tech_skill.routes.ts
+++ backend/src/routes/tech_skill.routes.ts
@@ -1,9 +1,17 @@
 import { Router } from "express";
 import { TechSkillController } from "@src/controllers";
+import ValidateMiddleware from "@src/middlewares/validate.middleware";
+import Requests from "@src/requests";
 
 const techSkillRouter = Router();
 const techSkillController = new TechSkillController();
 
 techSkillRouter.get("/", techSkillController.list);
+
+techSkillRouter.post(
+	"/",
+	ValidateMiddleware(Requests.CreateTechSkillRequest),
+	techSkillController.list,
+);
 
 export default techSkillRouter;
