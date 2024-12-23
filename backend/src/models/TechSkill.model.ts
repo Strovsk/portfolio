9dcc7dbce8ffe516194dccbd3013b868aeb439f7
Thiago Santa Clara <strovsk@outlook.com>
2024-12-22 23:06:33 -0300

    feat: add TechSkill model and export in models index

---
+++ backend/src/models/TechSkill.model.ts
@@ -0,0 +1,10 @@
+export default interface TechSkillModel {
+	id: number;
+	name: string;
+	link: string;
+	primary_color: string;
+	secondary_color: string;
+	short_description: string;
+	start_date: Date;
+	end_date: Date;
+}
