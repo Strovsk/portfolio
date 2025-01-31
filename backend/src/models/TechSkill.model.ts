21a2725922a183938fd295d97cafe1aa25ae710d
Thiago Santa Clara <strovsk@outlook.com>
2025-01-31 00:49:52 -0300

    refactor: update TechSkillModel to use camelCase for properties and change id type to string

--- backend/src/models/TechSkill.model.ts
+++ backend/src/models/TechSkill.model.ts
@@ -1,10 +1,10 @@
 export default interface TechSkillModel {
-	id: number;
+	id: string;
 	name: string;
 	link: string;
-	primary_color: string;
-	secondary_color: string;
-	short_description: string;
-	start_date: Date;
-	end_date: Date;
+	primaryColor: string;
+	secondaryColor: string;
+	shortDescription: string;
+	startDate: Date;
+	endDate: Date;
 }
