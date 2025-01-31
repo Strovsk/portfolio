6da58daf2739ef5c1465269c20b180a03553980d
Thiago Santa Clara <strovsk@outlook.com>
2025-01-31 00:59:57 -0300

    refactor: update CreateTechSkillRequest to use camelCase for property names

--- backend/src/requests/CreateTechSkillRequest.ts
+++ backend/src/requests/CreateTechSkillRequest.ts
@@ -4,11 +4,11 @@ import { z } from "zod";
 const CreateTechSkillRequest: z.ZodType = z.object({
 	name: z.string().min(1).max(255),
 	link: z.string().min(1).max(255),
-	primary_color: z.string().min(1).max(255),
-	secondary_color: z.string().min(1).max(255),
-	short_description: z.string().min(1).max(255),
-	start_date: z.date(),
-	end_date: z.date(),
+	primaryColor: z.string().min(1).max(255),
+	secondaryColor: z.string().min(1).max(255),
+	shortDescription: z.string().min(1).max(255),
+	startDate: z.date(),
+	endDate: z.date(),
 }) satisfies z.ZodType<Omit<TechSkillModel, "id">>;
 
 export default CreateTechSkillRequest;
