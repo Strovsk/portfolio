8905d6fd1c103c10e990df4073550bd193efc429
Thiago Santa Clara <strovsk@outlook.com>
2025-03-04 00:03:57 -0300

    feat: add AuthorizateService for token validation and authorization handling

---
+++ backend/src/services/Auth/Authorizate.service.ts
@@ -0,0 +1,38 @@
+import Config from "@src/config/Config";
+import * as jwt from "jsonwebtoken";
+import httpStatus from "http-status-codes";
+
+export default function AuthorizateService(token: string | undefined): {
+	isAuthorized: boolean;
+	message: string;
+	status: number;
+} {
+	if (!token) {
+		return {
+			isAuthorized: false,
+			message: "Authorization not supplied",
+			status: httpStatus.BAD_REQUEST,
+		};
+	}
+
+	const config = new Config();
+
+	try {
+		const data = jwt.verify(token, config.env.secret) as jwt.JwtPayload;
+		return { isAuthorized: true, message: data.data, status: httpStatus.OK };
+	} catch (error) {
+		if (error instanceof jwt.TokenExpiredError) {
+			return {
+				isAuthorized: false,
+				message: "Token expired",
+				status: httpStatus.UNAUTHORIZED,
+			};
+		}
+
+		return {
+			isAuthorized: false,
+			message: "Invalid token",
+			status: httpStatus.UNAUTHORIZED,
+		};
+	}
+}
