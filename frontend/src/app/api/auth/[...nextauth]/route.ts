26056dfd32d719449ae5711554c2942f26f0d4de
Thiago Santa Clara <strovsk@outlook.com>
2025-06-19 11:28:39 -0300

    style: remove unnecessary blank line in auth route file

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,6 @@ import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { cookies } from "next/headers";
 
-
 const handler = NextAuth({
 	pages: {
 		signIn: "/login",
