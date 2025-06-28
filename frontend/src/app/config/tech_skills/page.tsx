bb398ea4eb26e1af4bf2264dc0e32b7a47ccae4b
Thiago Santa Clara <strovsk@outlook.com>
2025-06-28 02:25:51 -0300

    feat: refactor TechSkillsPage to use Box and Typography components for layout and styling

--- frontend/src/app/config/tech_skills/page.tsx
+++ frontend/src/app/config/tech_skills/page.tsx
@@ -1,4 +1,7 @@
-import { listTechSkills } from "@/services/hooks/TechSkill.service";
+import TechSkillCard from "@/components/Pages/TechSkillsConfig/TechSkillCard/TechSkillCard";
+import { listTechSkills } from "@/services/TechSkill/TechSkill.service";
+import { Box, Button, Grid2, Typography } from "@mui/material";
+import React from "react";
 
 export default async function TechSkillsPage() {
 	const techLists = await listTechSkills();
@@ -6,11 +9,48 @@ export default async function TechSkillsPage() {
 	console.log("Tech Skills Page - techLists:", techLists);
 
 	return (
-		<div>
-			<h1>Tech Skills (protected page)</h1>
-			<p>This page is under construction.</p>
+		<Box
+			data-name="tech-skills-container"
+			sx={{
+				paddingInline: "80px",
+				paddingBlock: "100px",
+				display: "flex",
+				rowGap: "70px",
+				flexFlow: "column nowrap",
+			}}
+		>
+			<Typography variant="h2" fontWeight={300} color="#000">
+				Tech Skills
+			</Typography>
+
+			<Box
+				display={"flex"}
+				flexDirection={"row"}
+				gap={"16px"}
+				justifyContent={"flex-end"}
+				width={"100%"}
+			>
+				<Button variant="contained" color="primary">
+					Create
+				</Button>
+			</Box>
+
+			<Grid2 display={"flex"} container flexDirection={"row"} gap={"16px"}>
+				{techLists.map((tech) => (
+					<TechSkillCard
+						key={tech.id}
+						name={tech.name}
+						primaryColor={tech.primaryColor}
+						secondaryColor={tech.secondaryColor}
+						link={tech.link}
+						description={tech.shortDescription}
+						startDate={tech.startDate}
+						endDate={tech.endDate}
+					/>
+				))}
+			</Grid2>
 
 			<pre>{JSON.stringify(techLists, null, 2)}</pre>
-		</div>
+		</Box>
 	);
 }
