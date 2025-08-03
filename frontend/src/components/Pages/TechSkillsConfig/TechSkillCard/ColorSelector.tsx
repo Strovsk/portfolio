1a245cf26c6652fa7f7fb6f18f5f20c6150fa3a7
Thiago Santa Clara <strovsk@outlook.com>
2025-08-03 02:22:10 -0300

    feat(TechSkillCard): add color picker

--- frontend/src/components/Pages/TechSkillsConfig/TechSkillCard/ColorSelector.tsx
+++ frontend/src/components/Pages/TechSkillsConfig/TechSkillCard/ColorSelector.tsx
@@ -1,4 +1,9 @@
-import { Box, Typography } from "@mui/material";
+import { Box, ClickAwayListener, Fade, Typography } from "@mui/material";
+import React from "react";
+import { Sketch } from "@uiw/react-color";
+import { keyframes } from "@emotion/react";
+
+const bounceAnimation = keyframes`0% { transform: translateY(0) scaleY(0.9); } 100% { transform: translateY(-3px) scaleY(1); }`;
 
 interface ColorSelectorProps {
 	label: string;
@@ -6,32 +11,52 @@ interface ColorSelectorProps {
 }
 
 export const ColorSelector = (props: ColorSelectorProps) => {
+	const [openColorPicker, setOpenColorPicker] = React.useState(false);
+
 	return (
-		<Box
-			display="flex"
-			flexDirection="column"
-			alignItems="flex-start"
-			gap="5px"
-		>
-			<Typography color="primary" variant="caption">
-				{props.label}
-			</Typography>
+		<ClickAwayListener onClickAway={() => setOpenColorPicker(false)}>
 			<Box
 				display="flex"
-				flexDirection="row"
-				alignItems="center"
-				columnGap="10px"
+				flexDirection="column"
+				alignItems="flex-start"
+				gap="5px"
+				sx={{ position: "relative" }}
 			>
+				<Fade in={openColorPicker} timeout={300}>
+					<Box sx={{ position: "absolute", top: "100%", zIndex: 7 }}>
+						<Sketch color={props.color} />
+					</Box>
+				</Fade>
+				<Typography color="primary" variant="caption">
+					{props.label}
+				</Typography>
 				<Box
-					width={20}
-					height={20}
-					borderRadius="50%"
-					bgcolor={props.color}
-					border="1px solid #ccc"
-					boxShadow={"0 0 3px rgba(0, 0, 0, .23)"}
-				/>
-				<Typography sx={{ color: "#123123" }}>{props.color}</Typography>
+					display="flex"
+					flexDirection="row"
+					alignItems="center"
+					columnGap="10px"
+				>
+					<Box
+						width={20}
+						height={20}
+						borderRadius="50%"
+						bgcolor={props.color}
+						border="1px solid #ccc"
+						sx={{
+							cursor: "pointer",
+							animation: openColorPicker
+								? `${bounceAnimation} 0.7s infinite alternate`
+								: "none",
+							boxShadow: openColorPicker
+								? "3px 3px 5px rgba(81, 81, 81, .5)"
+								: "none",
+							transition: "background-color 0.3s, box-shadow 0.3s",
+						}}
+						onClick={() => setOpenColorPicker(!openColorPicker)}
+					/>
+					<Typography sx={{ color: "#123123" }}>{props.color}</Typography>
+				</Box>
 			</Box>
-		</Box>
+		</ClickAwayListener>
 	);
 };
