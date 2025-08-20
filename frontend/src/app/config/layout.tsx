ab88d409c8bd0e06e6c3deea9f0ea7cbd7576bc4
Thiago Santa Clara <strovsk@outlook.com>
2025-08-20 01:22:12 -0300

    feat: add react query devtools integration

--- frontend/src/app/config/layout.tsx
+++ frontend/src/app/config/layout.tsx
@@ -1,4 +1,5 @@
 import SideBarMenu from "@/components/Common/Menu/SideBarMenu";
+import { Providers } from "@/providers/Providers";
 import { Box, Grid2 } from "@mui/material";
 
 export default function ConfigLayout({
@@ -7,14 +8,24 @@ export default function ConfigLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<Grid2 display={"flex"} flexDirection="row" gap={2} width={'100vw'} height={'100vh'}>
-			<Box>
-                <SideBarMenu options={[
-                    { label: "Tech Skills", path: "/config/tech_skills" },
-                    { label: "Projects", path: "/config/projects", disabled: true }
-                ]} />
-            </Box>
-			<Box>{children}</Box>
-		</Grid2>
+		<Providers>
+			<Grid2
+				display={"flex"}
+				flexDirection="row"
+				gap={2}
+				width={"100vw"}
+				height={"100vh"}
+			>
+				<Box>
+					<SideBarMenu
+						options={[
+							{ label: "Tech Skills", path: "/config/tech_skills" },
+							{ label: "Projects", path: "/config/projects", disabled: true },
+						]}
+					/>
+				</Box>
+				<Box>{children}</Box>
+			</Grid2>
+		</Providers>
 	);
 }
